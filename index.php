954a6b6a33ee44ea228068e0dd69467e0736cb2b
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-09-17 07:40:04 +0300

    VIEWS: The Book View

--- index.php
+++ index.php
@@ -3,4 +3,13 @@
     * This files handles all the autoloading from the Composer code
 */
 require_once __DIR__.'/vendor/autoload.php';
+$loader = new Twig_loader_Filesystem(__DIR__.'/src/view');
+$twig = new Twig_Environment($loader);
+
+$bookModel = new BookModel(Db::getInstance());
+$book = $bookModel->get(1);
+
+$params = ['book' => $book];
+
+echo $twig->loadTemplate('book.twig')->render($params);
 ?>
