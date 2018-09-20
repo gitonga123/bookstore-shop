8c12374cdaf110100f7ac6699cfbd99ee718ea37
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-09-20 07:20:26 +0300

    Intergrate with PHPUnit library

--- test.php
+++ test.php
@@ -32,3 +32,5 @@ echo $child->sayHi() ."\n";
 // Integration tests = their aim is to verfiy that all the pieces of your application work together, so their scope is not limited to a class || function, but rather includes a set of classes or the whole application.
 // Accepatance test: they try to test a whole functionalty from the user's point of view.
 // code coverage = the amount of code that our tests execute, that is, the percentage of tested code. 
+
+// PHPUnit is a framework that provides a set of tools to write tests in a manner that gives us the ability to run tests automatically and delivers useful feedback to the developer.
