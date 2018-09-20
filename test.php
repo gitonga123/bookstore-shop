abaae99c254c77bb79e85231a037b95dd9c50675
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-09-20 07:16:02 +0300

    Introduction to Testing

--- test.php
+++ test.php
@@ -23,3 +23,12 @@ $child = new Child();
 
 echo $pops->sayHi() . "\n";
 echo $child->sayHi() ."\n";
+
+// testing
+// using pHPUnit
+// AEIOU principles;
+// Automatic, Extensive, Immediate, Open, Useful.
+// Unit tests = test a single class, or method, isolating them from the rest of the code.,
+// Integration tests = their aim is to verfiy that all the pieces of your application work together, so their scope is not limited to a class || function, but rather includes a set of classes or the whole application.
+// Accepatance test: they try to test a whole functionalty from the user's point of view.
+// code coverage = the amount of code that our tests execute, that is, the percentage of tested code. 
