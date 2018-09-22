4fe97a851422f369934847dfe90d3ef7c7c57650
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-09-22 21:11:14 +0300

    Test CustomerFactory:: InstanceOf()

--- tests/Domain/Customer/CustomerFactoryTest.php
+++ tests/Domain/Customer/CustomerFactoryTest.php
@@ -1,1 +1,24 @@
-CustomerFactoryTest.php
+<?php
+
+namespace Bookstore\Tests\Domain\Customer;
+
+use Bookstore\Domain\Customer\CustomerFactory;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Test for CustomerFactory Class
+ */
+class CustomerFactory extends PHPUnit_Framework_TestCase
+{
+	
+	public function testFactoryBasic()
+	{
+		$customer = CustomerFactory::factory(
+			'basic', 1, 'han', 'solo', 'han@solo.com'
+		);
+		$this->assertInstanceOf(
+			Basic::class, $customer, 'basic should create a Customer\Basic object.'
+		);
+	}
+}
+?>
