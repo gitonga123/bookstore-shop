6f99b5db0d01405249d11c04b5d920f6fcb1892b
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-09-23 20:28:26 +0300

    PHPUnit: Data Providers

--- tests/Domain/Customer/CustomerFactoryTest.php
+++ tests/Domain/Customer/CustomerFactoryTest.php
@@ -33,5 +33,40 @@ class CustomerFactoryTest extends \PHPUnit_Framework_TestCase
 	{
 		$customer = CustomerFactory::factory('deluxe', 1, 'han','solo', 'han@solo.com');
 	}
+
+	public function providerFactoryValidCustomerTypes()
+	{
+		return[
+            'Basic customer, lowercase' => [
+            	'type' => 'basic',
+            	'expectedType' => '\Bookstore\Domain\Customer\Basic'
+            ],
+            'Basic customer, uppercase' => [
+            	'type' => 'BASIC',
+            	'expectedType' => '\Bookstore\Domain\Customer\Basic'
+            ], 
+            'Premium customer, lowercase' => [
+            	'type' => 'premium',
+            	'expectedType' => '\Bookstore\Domain\Customer\Premium'
+            ], 
+            'Premium customer, uppercase' => [
+            	'type' => 'PREMIUM',
+            	'expectedType' => '\Bookstore\Domain\Customer\Premium'
+            ] 
+		];
+	}
+    
+    /**
+	 * @dataProvider providerFactoryValidCustomerTypes
+	 * @param string $type
+	 * @param string $expectedType
+	*/
+
+	public function testFactoryValidCustomerTypes(string $type, string $expectedType)
+	{
+		$customer = CustomerFactory::factory($type, 1, 'han', 'solo', 'han@solo.com');
+		$this->assertInstanceOf($expectedType, $customer, 'Factory Created the wrong type of customer');
+	}
+
 }
 ?>
