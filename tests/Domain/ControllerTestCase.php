5d3494afd7a063d6d9f45c838cc323abc37f137f
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-09-24 08:26:42 +0300

    PHPUnit: Mock builder

---
+++ tests/Domain/ControllerTestCase.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Bookstore\Tests;
+
+use Bookstore\Core\Config;
+use Bookstore\Utils\DependencyInjector;
+use Monolog\Logger;
+use Twig_Environment;
+use Twig_Loader_Filesystem;
+use PDO;
+
+abstract class ControllerTestCase extends AbstractTestCase
+{
+	protected $di;
+
+	public function setUp()
+	{
+		$this->di = new DependencyInjector();
+		$this->di->set('PDO', $this->mock(PDO::class));
+		$this->di->set('Utills\Config', $this->mock(Config::class));
+		$this->di->set(
+			'Twig_Environment',
+			$this->mock(Twig_Environment::class)
+		);
+		$this->di->set('Logger', $this->mock(Logger::class));
+	}
+}
+?>
